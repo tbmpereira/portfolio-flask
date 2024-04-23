6c1ed66194a3ebe4d9b5661b947ba201ecaab589
tbmpereira <mapereira@ufmg.br>
2024-04-22 22:25:17 -0300

    Add animation styles for data-anime elements and scroll animation in script.js

--- static/script.js
+++ static/script.js
@@ -23,3 +23,21 @@ navItem.forEach(item => {
         }
     });
 })
+
+const item = document.querySelectorAll('[data-anime]');
+
+const animeScroll = () => {
+    const windowTop = window.pageYOffset + window.innerHeight * 0.85;
+
+    item.forEach((element) => {
+        if(windowTop > element.offsetTop){
+            element.classList.add('animate');
+        } else {
+            element.classList.remove('animate');
+        }
+    });
+}
+
+window.addEventListener('scroll', () => {
+    animeScroll();
+})
