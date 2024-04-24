078a20799d6944b572fdd903f928995c748577bf
tbmpereira <mapereira@ufmg.br>
2024-04-24 13:26:14 -0300

    footer e config deploy

--- app.py
+++ app.py
@@ -1,56 +1,13 @@
-from flask import Flask, render_template, redirect, request, flash
-from flask_mail import Mail, Message
-from config import username, password
+from flask import Flask, render_template
+#from dotenv import load_dotenv
 
 app = Flask(__name__)
 app.secret_key = "tbmpereira"
 
-mail_settings = {
-    "MAIL_SERVER": 'smtp.mailgun.org',
-    "MAIL_PORT": 587,
-    "MAIL_USE_TLS": True,
-    "MAIL_USE_SSL": False,
-    "MAIL_USERNAME": username,
-    "MAIL_PASSWORD": password
-}
-
-app.config.update(mail_settings)
-mail = Mail(app)
-
-class Contato:
-    def __init__(self, nome, email, mensagem):
-        self.nome = nome
-        self.email = email
-        self.mensagem = mensagem
 
 @app.route('/')
 def index():
     return render_template('index.html')
-
-
-@app.route('/send', methods=['POST', 'GET'])
-def send():
-    if request.method == 'POST':
-        nome = request.form['nome']
-        email = request.form['email']
-        mensagem = request.form['mensagem']
-
-        contato = Contato(nome, email, mensagem)
-
-        msg = Message(
-            subject= f'{nome} te mandou uma mensagem pelo site!',
-            sender= app.config.get("MAIL_USERNAME"),
-            recipients=['mpereiraufv@gmail.com'],
-            body=f'''
-            Nome: {contato.nome}
-            Email: {contato.email}
-            Mensagem: {contato.mensagem}
-            '''
-        )
-
-        mail.send(msg)
-        flash('Mensagem enviada com sucesso!')
-    return redirect('/')
 
 if __name__ == '__main__':
     app.run(debug=True)
